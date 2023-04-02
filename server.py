112d0b9ef82452d925fd2f02d02e517521e13063
marwan-rouissi <115158061+marwan-rouissi@users.noreply.github.com>
2023-04-02 15:38:21 +0200

    Improved server

--- server.py
+++ server.py
@@ -19,13 +19,12 @@ FORMAT = "utf-8"
 #
 # instancier un objet base de donnée
 database = BDD()
-#
 
 
 class Server:
+	# création d'un objet Server
 
 	def __init__(self) -> None:
-		
 		# Création d'un socket serveur
 		self.server = socket.socket(socket.AF_INET,
 							socket.SOCK_STREAM)
@@ -47,44 +46,34 @@ class Server:
 
 		while True:
 
-			# accept connections and returns
-			# a new connection to the client
-			# and the address bound to it
+			# accepter les connexions et retourner une nouvelle connexion au client et à l'adresse à laquelle il est lié
 			conn, addr = self.server.accept()
 			conn.send("NAME".encode(FORMAT))
 
-			try:
-				# 1024 represente le nombre max de data (bytes) pouvant être recu
-				self.name = conn.recv(1024).decode(FORMAT)
+			# 1024 represente le nombre max de data (bytes) pouvant être recu
+			self.name = conn.recv(1024).decode(FORMAT)
 
-				# ajouter le client et son pseudo aux listes dédiées
-				self.names.append(self.name)
-				self.clients.append(conn)
+			# ajouter le client et son pseudo aux listes dédiées
+			self.names.append(self.name)
+			self.clients.append(conn)
 
-				print(f"Name is :{self.name}")
+			print(f"Name is :{self.name}")
 
-				# diffuser l'historique à tout les clients connectés 
-				self.broadcastMessage(self.displayHistory())
+			# diffuser l'historique à tout les clients connectés 
+			self.broadcastMessage(self.displayHistory())
 
-				# broadcast message
-				self.broadcastMessage(f"\n{self.name} has joined the chat!\n\n".encode(FORMAT))
+			# broadcast message
+			self.broadcastMessage(f"\n{self.name} has joined the chat!\n\n".encode(FORMAT))
 
-				# initialisation et lancement du thread de gestion des msgs entrants
-				thread = threading.Thread(target=self.handle,
-										args=(conn, addr))
-				thread.start()
+			# initialisation et lancement du thread de gestion des msgs entrants
+			thread = threading.Thread(target=self.handle,
+									args=(conn, addr))
+			thread.start()
 
-				# nombre de clients connectés au serveur
-				print(f"active connections {threading.active_count()-1}")
-				print(f"clients: {self.clients}")
-				print(f"pseudo: {self.names}")
-				
-			except:
-				# récupération de l'index du client déconnecté depuis la liste de pseudo actifs
-				client_index = self.names.index(self.name)
-				# supprimer le client déconnecté depuis de la liste à l'aide de son index
-				self.clients.pop(client_index)
-				print(f"clients: {self.clients}")
+			# nombre de clients connectés au serveur
+			print(f"active connections {threading.active_count()-1}\n")
+
+
 
 	# methode pour gérer les messages entrants
 	def handle(self, conn, addr):
@@ -100,17 +89,18 @@ class Server:
 				# sauvegarde du msg dans la BDD
 				self.saveHistory(message)
 
-
 		except:
-			print(f"erreur co de {self.name}")
+			# print(f"erreur co de {self.name}")
+			self.broadcastMessage(f"\n{self.name} has left the chat!\n\n".encode(FORMAT))
+			client_index = self.clients.index(conn)
 
-			client_index = self.names.index(self.name)
-			print(client_index)
 			self.names.pop(client_index)
-			# self.clients.pop(client_index)
+			# supprimer le client déconnecté depuis de la liste à l'aide de son index
+			self.clients.pop(client_index)
 
-			# close the connection
+			# fermer la connexion
 			conn.close()
+
 
 	# méthode pour sauvegarder les messages échangés dans la BDD
 	def saveHistory(self, message):
@@ -144,9 +134,11 @@ class Server:
 
 	# methode pour diffuser le msg à chacun des clients du serveur
 	def broadcastMessage(self, message):
-		for client in self.clients:
-			client.send(message)
-
+		try:
+			for client in self.clients:
+				client.send(message)
+		except:
+			print("Un client vient de se déconnecter.")
 
 if __name__ == "__main__":
 	serv = Server()
